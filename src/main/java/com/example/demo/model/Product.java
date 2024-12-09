0178f4c11cf8f31b5b10077351dc6910f90eabd7
saurabhtipule <tipulesaurabh12@gmail.com>
2024-12-09 14:48:52 +0530

    city variable added

--- src/main/java/com/example/demo/model/Product.java
+++ src/main/java/com/example/demo/model/Product.java
@@ -5,6 +5,9 @@ import javax.persistence.Id;
 
 @Entity
 public class Product {
+
+	private String city;
+
 	@Id
 	private int pid;
 	private String pname;
@@ -12,44 +15,53 @@ public class Product {
 	private int pquantity;
 	private String puname;
 	private String pwd;
+
 	public int getPid() {
 		return pid;
 	}
+
 	public void setPid(int pid) {
 		this.pid = pid;
 	}
+
 	public String getPname() {
 		return pname;
 	}
+
 	public void setPname(String pname) {
 		this.pname = pname;
 	}
+
 	public int getPprice() {
 		return pprice;
 	}
+
 	public void setPprice(int pprice) {
 		this.pprice = pprice;
 	}
+
 	public int getPquantity() {
 		return pquantity;
 	}
+
 	public void setPquantity(int pquantity) {
 		this.pquantity = pquantity;
 	}
+
 	public String getPuname() {
 		return puname;
 	}
+
 	public void setPuname(String puname) {
 		this.puname = puname;
 	}
+
 	public String getPwd() {
 		return pwd;
 	}
+
 	public void setPwd(String pwd) {
 		this.pwd = pwd;
 	}
-	
-	
-	
-}
 
+}
